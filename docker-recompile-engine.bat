f3008cfb5096602cbc53853255e5c7e832f8b22f
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2020-10-05 10:40:31 -0400

    Make recompile script generic.

--- docker-recompile-engine.bat
+++
@@ -1,4 +0,0 @@
-docker stop ms-engine
-docker rm ms-engine
-docker-compose -f docker-compose.yml build --no-cache engine
-docker-compose -f docker-compose.yml up -d engine
---
+++ docker-recompile.bat
@@ -0,0 +1,4 @@
+docker stop ms-%1
+docker rm ms-%1
+docker-compose -f docker-compose.yml build --no-cache %1
+docker-compose -f docker-compose.yml up -d %1
